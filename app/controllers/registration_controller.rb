3d98cc705c759557f5bf6f811570942a5163ffbf
PeterPodniesinski <ppodnies@andrew.cmu.edu>
2014-04-22 15:39:28 -0400

    Added registration crud action code to controller

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,23 +1,49 @@
 class RegistrationController < ApplicationController
   before_action :set_registration, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @deposit_registrations = Registration.deposit_only.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @full_registrations = Registration.paid.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  def show
+  end
+
   def new
-  end
-
-  def create
-  end
-
-  def update
+    @registration = Registration.new
   end
 
   def edit
   end
 
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      redirect_to @registration, notice: "#{@registration} was added to the system."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @registration.update(registration_params)
+      redirect_to @registration, notice: "#{@registration} was revised in the system."
+    else
+      render action: 'edit'
+    end
+  end
+
   def destroy
+    @registration.destroy
+    redirect_to registrations_url, notice: "#{@registration} was removed from the system."
   end
 
-  def index
-  end
+  private
+    def set_registration
+      @registration = Registration.find(params[:id])
+    end
 
-  def show
-  end
+    def registration_params
+      params.require(:registration).permit(:camp_id, :student_id, :payment_status, :points_earned)
+    end
 end
