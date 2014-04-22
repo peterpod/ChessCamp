31240c7374f36d12c70517361f170c4cedd6a5c3
PeterPodniesinski <ppodnies@andrew.cmu.edu>
2014-04-22 15:43:40 -0400

    Added family crud action code to controller

--- app/controllers/family_controller.rb
+++ app/controllers/family_controller.rb
@@ -1,23 +1,49 @@
 class FamiliyController < ApplicationController
   before_action :set_family, only: [:show, :edit, :update, :destroy]
+ 
+  def index
+    @active_families = Family.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @inactive_families = Family.inactive.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  def show
+  end
+
   def new
-  end
-
-  def create
-  end
-
-  def update
+    @family = Family.new
   end
 
   def edit
   end
 
+  def create
+    @family = Family.new(family_params)
+    if @family.save
+      redirect_to @family, notice: "#{@family.family_name} was added to the system."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @family.update(family_params)
+      redirect_to @family, notice: "#{@family.family_name} was revised in the system."
+    else
+      render action: 'edit'
+    end
+  end
+
   def destroy
+    @family.destroy
+    redirect_to familys_url, notice: "#{@family.family_name} was removed from the system."
   end
 
-  def index
-  end
+  private
+    def set_family
+      @family = Family.find(params[:id])
+    end
 
-  def show
-  end
+    def family_params
+      params.require(:family).permit(:family_name, :parent_first_name, :email, :phone, :active)
+    end
 end
