8e2f5090d1926a9e086fd38f0a7c0c5e6c438d83
PeterPodniesinski <ppodnies@andrew.cmu.edu>
2014-04-25 12:39:42 -0400

    Added images from phase 3 then 2 layouts to shape the home pages as well as content with a static footer.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+ layout :resolve_layout
   def index
   end
 
@@ -11,4 +12,26 @@ class HomeController < ApplicationController
   def privacy
   end
   
+  def search
+    @query = params[:query]
+    @camp = Camp.search(@query)
+    @curriculums = Curriculum.search(@query)
+    @instructor = Instructor.search(@query)
+    @total_hits = @camp.size + @instructor.size + @curriculums.size
+  end
+
+  private
+
+  def resolve_layout
+    case action_name
+    #When Home page displays index page. I have a default layout
+    #specified. All other home pages inherit from 'home2' layout
+    when "index"
+      "home"
+    else
+      "home2"
+    end
+  end
+
 end
+
