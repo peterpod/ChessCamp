40f4e9e212ad1a9b12d4089de9fc208ab42e4353
PeterPodniesinski <ppodnies@andrew.cmu.edu>
2014-04-27 12:29:57 -0400

    Finalizing minor changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
+    if user && User.authenticate(params[:username],params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
